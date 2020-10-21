f8d1786f83ccf05eac78a9b377b6ce5b93942b7e
Shom Bandopadhaya <shom@bandopadhaya.com>
2020-10-20 19:53:42 -0600

    inital commit, generate and add key to openpgp

---
+++ keyoxidizer.sh
@@ -0,0 +1,66 @@
+#!/usr/bin/env bash
+set -euo pipefail
+echo "Keyoxidizer - Interactive Keyoxide helper"
+
+# GPG config file for unattended keygen
+generateConfig()
+{
+cat > ./keyoxidizer.config<<EOF
+#%dry-run
+Key-Type: RSA
+Key-Length: 2048
+Subkey-Type: RSA
+Name-Real: $keyoxidizer_name
+Name-Email: $keyoxidizer_email
+Name-Comment: $keyoxidizer_comment
+Expire-Date: 2y
+%ask-passphrase
+%commit
+%echo done
+EOF
+}
+
+# Generate and upload
+newKey()
+{
+   echo "Generating new key..."
+   read -p "Enter full name: " keyoxidizer_name
+   read -p "Enter email: " keyoxidizer_email
+   read -p "Enter comment about key: " keyoxidizer_comment
+   generateConfig
+   echo "You'll now be asked to enter a password to secure the key"
+   sleep 1.5s
+   gpg --batch --generate-key ./keyoxidizer.config
+   gpg --fingerprint #clears out meta output
+
+   echo -e "\n\n\\n==================================================\n"
+   echo -e "Here's your GPG fingerprint: \n"
+   gpg --with-colons --fingerprint $keyoxidizer_name | \
+      awk -F: '$1 == "fpr" {print $10;}'| \
+      sed -n '1p'
+   echo -e "==================================================\n\n"
+}
+
+# Export key to keys.OpenPGP.org
+exportOpenPGP()
+{
+   gpg --export $keyoxidizer_email | \
+      curl -T - https://keys.openpgp.org
+}
+
+# Operate on existing key
+existingKey()
+{
+   echo "Loading existing key not yet supported."
+}
+
+# User request handling
+echo -e "Select an option: \n1. Create a new key. \n2. Use an existing key."
+read keyoxidizer_keyType
+
+if [ "$keyoxidizer_keyType" == "1" ]; then
+   newKey
+   exportOpenPGP
+else
+   existingKey
+fi
