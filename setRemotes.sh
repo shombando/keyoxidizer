c9110cb7110f4be62d5dc3198bdc94ddcf60b10b
Shom Bandopadhaya <shom@bandopadhaya.com>
2025-03-03 11:25:11 -0600

    Switching to Codeberg as primary

--- setRemotes.sh
+++ setRemotes.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/sh
 set -eu
+git remote set-url --push origin --add git@codeberg.org:shom/keyoxidizer.git 
 git remote set-url --push origin --add git@git.sr.ht:~shom/keyoxidizer
 git remote set-url --push origin --add git@github.com:shombando/keyoxidizer.git
-git remote set-url --push origin --add git@codeberg.org:shom/keyoxidizer.git
